ed5c1492692788ea7803f69b419b03e07d658eb4
Caio Machado Trotta <contatotrotta@gmail.com>
2022-04-08 16:46:14 -0300

    Terceira Aula
    Terceira Aula de PHP

---
+++ _footer.php
@@ -0,0 +1,16 @@
+</main>
+
+<footer>
+
+    <a href="/" title="Pagina Inicial"><i class="fa-solid fa-house-chimney"></i></a>
+
+    <div>&copy; Copyright 2022 Vitugo.</div>
+
+    <a href="#top" title="Topo da pagina"><i class="fa-solid fa-circle-up"></i></a>
+
+</footer>
+
+</div>
+
+</body>
+</html>
---
+++ _header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Vitugo</title>
+</head>
+<body>
+
+    <a id="top"></a>
+    
+    <div class="wrap">
+
+        <head> 
+            <a href="/" title="Pagina Inicial"><img src="/img/logo01.png" alt="Logo tipo de Vitugo"></a>
+        </head>
+
+        <h1>Vitugo<small>Quem tem fome tem pressa.</small></h1>
+
+        <nav>
+            
+            <a href="/" title="Inicio"><i class="fa-solid fa-house-chimney"></i><span>Início</span></a>
+           
+            <a href="/contatos" title="Contatos"><i class="fa-solid fa-comment-dots"></i><span>Contatos</span></a>
+
+            <a href="/sobre" title="Sobre"><i class="fa-solid fa-circle-info"></i><span>Sobre</span></a>
+
+            <a href="/usuario" title="Usuário"><i class="fa-solid fa-user"></i><span>Usuário</span></a>
+
+        </nav>
+
+        <main>
+            
