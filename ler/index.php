f33029d32b6c3e313506b21a5a5e6563f4d6371f
Caio Machado Trotta <contatotrotta@gmail.com>
2022-04-25 16:35:52 -0300

    Aula 08 parte 2

---
+++ ler/index.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Arquivo que faz a configuração incial da página.
+ * Por exemplo, conecta-se ao banco de dados.
+ * 
+ * A superglobal "$_SERVER['DOCUMENT_ROOT']" retorna o caminho da raiz do site no Windows.
+ * Ex.: C:\xampp\htdocs 
+ *     Referências:
+ *     → https://www.w3schools.com/php/php_includes.asp
+ *     → https://www.php.net/manual/pt_BR/function.include.php
+ *     → https://www.php.net/manual/pt_BR/language.variables.superglobals.php
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
+
+/**
+ * Variável que define o título desta página.
+ */
+$title = "Quem tem fome tem pressa...";
+
+
+/***********************************************
+ * Seus códigos PHP desta página iniciam aqui! *
+ ***********************************************/
+
+    // Obtém o ID do artido da Página
+    if( isset ($_GET ['id']) )  $id = intval($_GET ['id']);
+    else $id =0;
+
+    // Se está tentando acessar de forma incorreta, retorna para a index.
+    if ($id === 0) header('LocaTION: /index.php');
+
+    // Query que obtém o artigo
+    $sql = <<<SQL
+    SELECT *,
+	DATE_FORMAT(art_date, '%d/%m/%Y às %H:%i') AS date_br,
+    DATE_FORMAT(user_birth, '%d/%m/%Y') AS birth_br
+    FROM `articles`
+    INNER JOIN `users` ON art_author = user_id
+    WHERE art_id = '{$id}'
+    AND art_status = 'on'
+    AND art_date <= NOW();
+
+SQL;
+
+    // Executa a query
+    $res = $conn->query($sql);
+
+    // Se não retornou um (1) artigo, volta para a index
+
+    if ($res->num_rows != 1) header('LocaTION: /index.php');
+
+    // Tudo certo, vamos obter os dados do registro obtido
+    $artigo = $res->fetch_assoc();
+
+    print_r($artigo);
+    exit;
+   
+/**
+ * Tarefas:
+ * Eibir o perfil do autor na aside
+ * Exibir mais artigos do autor
+ * Exibir a idade do autor (tarefa de casa)
+ */
+
+/************************************************
+ * Seus códigos PHP desta página terminam aqui! *
+ ************************************************/
+
+/**
+ * Inclui o cabeçalho da página.
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
+
+?>
+
+<section>
+
+    <h2>Título da página</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste aliquam aperiam voluptatem molestias nemo odit unde modi cupiditate exercitationem doloremque quaerat soluta rerum quidem dignissimos officiis sapiente, aut alias!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio soluta voluptatum consequatur voluptatibus cupiditate temporibus qui, nostrum deserunt minus laudantium in officia rem dignissimos facilis modi culpa error aliquam? Quam?</p>
+
+</section>
+
+<aside>
+
+    <h3>Lateral</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur iusto.</p>
+
+</aside>
+
+<?php
+
+/**
+ * Inclui o rodapé da página.
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_footer.php');
